00a03d087ade91f3dff38f0f1dfaf7f75e16962a
Fani Ehsan <FANIEH@de.softlab.net>
2023-08-21 15:57:29 +0200

    added ngrx setup

---
+++ Angular/ngrx ex/sampleprj/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CounterControlsComponent } from './counter-controls/counter-controls/counter-controls.component';
+import { CounterOutputComponent } from './counter-ouput/counter-output/counter-output.component';
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './store/counter.reducer';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CounterControlsComponent,
+    CounterOutputComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot({
+      counter:counterReducer
+    }, {})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
